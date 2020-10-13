1513875be6ef755bf40f700b8112ffbd20517ca2
rkrazaka <Zava@Zavakanto.home>
2020-10-14 00:21:07 +0200

    Read a file

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,39 +3,29 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+	ReadSymptomDataFromFile readSymptomDataFromFile;
+	private static String symptomsfile  = "symptoms.txt";
+	
+	public AnalyticsCounter() {
+		readSymptomDataFromFile = new ReadSymptomDataFromFile(symptomsfile);
 	}
+
+	private List<String> reader() {
+		return readSymptomDataFromFile.GetSymptoms();
+	}
+	
+	public Map<String, Integer> counter() {
+		return readSymptomDataFromFile.countNumberSymptoms(this.reader());
+	}
+	
+	public void outputFile(Map<String, Integer> res) throws IOException {
+		readSymptomDataFromFile.result(res);
+	}
+	
 }
