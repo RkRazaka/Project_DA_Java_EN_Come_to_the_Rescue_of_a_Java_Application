9c9b5cf8f59675370776b72c448e880618d65f73
rkrazaka <Zava@Zavakanto.home>
2020-10-16 00:54:51 +0200

    Main java

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -3,11 +3,20 @@ package com.hemebiotech.analytics;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * 
+ * @author OC
+ * @version : 1.0.2
+ * 
+ */
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		// Analyse "symtoms.txt" File
 		Map<String, Integer> countEstablished = analyticsCounter.counter();
+		// Put All Occurrence In "result.out" File
 		analyticsCounter.outputFile(countEstablished);
 
 	}
